c78392d13b86c9c3aac1fc9c3a609d87654e70b0
jerstnd <74831814+jerstnd@users.noreply.github.com>
2021-01-20 13:40:56 +0700

    Add files via upload

---
+++ binarysearch.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+bool search(int low, int high){
+    int randNum = rand() % 100;
+    bool found = false;
+
+    //logic
+    while(low <= high && !found){
+        int mid = (low+high) / 2;
+        if( mid == randNum){
+            found = true;
+        } else {
+            if(randNum > mid){
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+    }
+    return found;
+}
+
+//test
+int main() {
+    cout << search(1, 99) << endl;
+}
